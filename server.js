528f7b465975db56746b421557a5ea1d3648a6e6
nelson-singleton <nsingleton@ymail.com>
2020-09-30 15:53:38 -0500

    Just starting

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+//imports
 const express = require('express');
+var morgan = require('morgan')
 
+//native imports
+const router = require('./users/userRouter')
+
+//global variables
 const server = express();
+const logger = morgan('combined')
+
+//middleware
+server.use(express.json());
+server.use(logger)
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
